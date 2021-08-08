c659fd1c2618d7b51f69b08abf107e0db9cfde08
edenpardo2 <eden@salefreaks.com>
2021-08-08 16:50:25 +0300

    End of Section 6: Axios

--- client-app/src/Features/activities/dashboard/ActivityDashboard.tsx
+++ client-app/src/Features/activities/dashboard/ActivityDashboard.tsx
@@ -7,31 +7,55 @@ import ActivityForm from "../form/ActivityForm";
 
 interface Props {
   activities: Activity[];
-  selectedActivity:Activity | undefined;
-  selectActivity:(id:string)=>void;
-  cancelselectActivity:()=>void;
+  selectedActivity: Activity | undefined;
+  selectActivity: (id: string) => void;
+  cancelselectActivity: () => void;
   editMode: boolean;
-  openForm:(id:string)=>void;
-  closeForm: ()=>void;
-  createOrEdit: (activity:Activity)=>void;
-  deleteActivity:(id:string)=>void;
+  openForm: (id: string) => void;
+  closeForm: () => void;
+  createOrEdit: (activity: Activity) => void;
+  deleteActivity: (id: string) => void;
+  submitting: boolean;
 }
-export default function ActivityDashboard({ activities,selectedActivity,selectActivity,cancelselectActivity,editMode,openForm,closeForm,createOrEdit,deleteActivity}: Props) {
+export default function ActivityDashboard({
+  activities,
+  selectedActivity,
+  selectActivity,
+  cancelselectActivity,
+  editMode,
+  openForm,
+  closeForm,
+  createOrEdit,
+  deleteActivity,
+  submitting,
+}: Props) {
   return (
     <Grid>
       <Grid.Column width="10">
-        <ActivityList activities={activities} selectActivity={selectActivity} deleteActivity={deleteActivity}/>
+        <ActivityList
+          activities={activities}
+          selectActivity={selectActivity}
+          deleteActivity={deleteActivity}
+          submitting={submitting}
+        />
       </Grid.Column>
       <Grid.Column width="6">
-        {selectedActivity && !editMode &&
-        <ActivityDetails 
-            activity={selectedActivity} 
+        {selectedActivity && !editMode && (
+          <ActivityDetails
+            activity={selectedActivity}
             cancelselectActivity={cancelselectActivity}
             openForm={openForm}
-        />}
-        {editMode&&
-        <ActivityForm closeForm={closeForm} activity={selectedActivity} createOrEdit={createOrEdit}/>}
+          />
+        )}
+        {editMode && (
+          <ActivityForm
+            closeForm={closeForm}
+            activity={selectedActivity}
+            createOrEdit={createOrEdit}
+            submitting={submitting}
+          />
+        )}
       </Grid.Column>
     </Grid>
-  )
+  );
 }
