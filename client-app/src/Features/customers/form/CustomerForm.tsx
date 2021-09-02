74a5faa1958e385613e9147b63589f47394d4f7d
edenpardo2 <eden@salefreaks.com>
2021-09-02 11:10:13 +0300

    End of Section 11: Forms

--- client-app/src/Features/customers/form/CustomerForm.tsx
+++ client-app/src/Features/customers/form/CustomerForm.tsx
@@ -1,11 +1,18 @@
 import { observer } from "mobx-react-lite";
-import React, { ChangeEvent, SyntheticEvent, useState } from "react";
+import React, { useState } from "react";
 import { useEffect } from "react";
 import { Link, useHistory, useParams } from "react-router-dom";
-import { Button, Form, Segment } from "semantic-ui-react";
+import { Button, Header, Segment } from "semantic-ui-react";
 import LoadingComponent from "../../../app/layout/LoadingComponenet";
 import { useStore } from "../../../app/stores/store";
 import { v4 as uuid } from "uuid";
+import { Formik, Form } from "formik";
+import * as Yup from "yup";
+import MyTextInput from "../../../app/common/form/MyTextInput";
+import MyTextArea from "../../../app/common/form/MyTextArea";
+import MySelectInput from "../../../app/common/form/MySelectInput";
+import { customerTypeOptions } from "../../../app/common/options/customerTypeOptions";
+import { Customer } from "../../../app/models/customer";
 
 export default observer(function CustomerForm() {
   const history = useHistory();
@@ -17,14 +24,7 @@ export default observer(function CustomerForm() {
     loadCustomer,
     loadingInitial,
   } = customerStore;
-  
-  const customerTypeOptions = [
-    { key: 'P', text: 'פרטי', value: 'פרטי' },
-    { key: 'B', text: 'עסק', value: 'עסק' },
-    { key: 'I', text: 'מוסד', value: 'עסק' },
-    { key: 'D', text: 'דילר', value: 'דילר' },
-    { key: 'O', text: 'אחר', value: 'אחר' },
-  ]
+
   const { id } = useParams<{ id: string }>();
   const [customer, setCustomer] = useState({
     id: "",
@@ -40,149 +40,109 @@ export default observer(function CustomerForm() {
     bookkeepingPhoneNumber: "",
     moreInfo: "",
   });
+  const validationSchema = Yup.object({
+    mainEmail: Yup.string()
+      .email("כתובת מייל אינה חוקית")
+      .required("נא להכניס את המייל הראשי של הלקוח"),
+    mainPhoneNumber: Yup.string().required(
+      "נא להכניס את מספר הפלאפון של הלקוח"
+    ),
+    customerName: Yup.string().required("נא להכניס את שמו של הלקוח"),
+  });
 
   useEffect(() => {
     if (id) loadCustomer(id).then((customer) => setCustomer(customer!));
   }, [id, loadCustomer]);
 
-  function handleSubmit() {
+  function handleFormSubmit(customer: Customer) {
     if (customer.id.length === 0) {
       let newCustomer = {
         ...customer,
         id: uuid(),
       };
-      createCustomer(newCustomer).then(() =>
-        history.push(`/customers`)
-      );
+      createCustomer(newCustomer).then(() => history.push(`/customers`));
     } else {
-      updateCustomer(customer).then(() =>
-        history.push(`/customers`)
-      );
+      updateCustomer(customer).then(() => history.push(`/customers`));
     }
   }
-  function handleInputChange(
-    event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ) {
-    const selected_box =  event.target;
-    console.log(selected_box);
-    const { name, value } = event.target;
-    console.log(name);
-    setCustomer({ ...customer, [name]: value });
-  }
-
-  function handleSelectChange (event: SyntheticEvent<HTMLElement>,data: any) {
-    const { name, value } = data;
-    console.log(name);
-    console.log(value);
-    setCustomer({ ...customer, [name]: value });
-  };
 
   if (loadingInitial) return <LoadingComponent content="Loading customer..." />;
 
   return (
     <Segment clearing>
-      <Form onSubmit={handleSubmit} autoComplete="off">
-        <Form.Group widths="equal">
-          <Form.Input
-            label="חברה"
-            placeholder="חברה"
-            value={customer.company}
-            name="company"
-            onChange={handleInputChange}
-          />
-          <Form.Input
-            label="שם לקוח"
-            placeholder="שם לקוח"
-            value={customer.customerName}
-            name="customerName"
-            onChange={handleInputChange}
-          />
-          <Form.Input
-            label="מייל ראשי"
-            placeholder="מייל ראשי"
-            value={customer.mainEmail}
-            name="mainEmail"
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-        <Form.Group widths="equal">
-          <Form.Input
-            label="מספר פלאפון ראשי"
-            placeholder="מספר פלאפון ראשי"
-            value={customer.mainPhoneNumber}
-            name="mainPhoneNumber"
-            onChange={handleInputChange}
-          />
-          <Form.Input
-            label="כתובת"
-            placeholder="כתובת"
-            value={customer.address}
-            name="address"
-            onChange={handleInputChange}
-          />
-          <Form.Input
-            label="חפ"
-            placeholder="חפ"
-            value={customer.hp}
-            name="hp"
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-        <Form.Select
-          id="select_id"
-          width={5}
-          options={customerTypeOptions}
-          label="סוג לקוח"
-          placeholder="סוג לקוח"
-          value={customer.customerType}
-          name="customerType"
-          onChange={handleSelectChange}
-        />
-        <Form.Group widths="equal">
-          <Form.Input
-            label="הנהלת חשבונות-שם לקוח"
-            placeholder="הנהלת חשבונות-שם לקוח"
-            value={customer.bookkeepingName}
-            name="bookkeepingName"
-            onChange={handleInputChange}
-          />
-          <Form.Input
-            label="הנהלת חשבונות-מייל"
-            placeholder="הנהלת חשבונות-מייל"
-            value={customer.bookkeepingEmail}
-            name="bookkeepingEmail"
-            onChange={handleInputChange}
-          />
-          <Form.Input
-            label="הנהלת חשבונות-מספר פלאפון"
-            placeholder="הנהלת חשבונות-מספר פלאפון"
-            value={customer.bookkeepingPhoneNumber}
-            name="bookkeepingPhoneNumber"
-            onChange={handleInputChange}
-          />
-        </Form.Group>
-        <Form.TextArea
-          label="מידע נוסף"
-          placeholder="מידע נוסף"
-          value={customer.moreInfo}
-          name="moreInfo"
-          onChange={handleInputChange}
-        />
-        <Button
-          loading={loading}
-          floated="right"
-          positive
-          type="submit"
-          content="שמור"
-        />
-        <Button
-          as={Link}
-          to="/customers"
-          floated="right"
-          type="button"
-          content="ביטול"
-        />
-      </Form>
+      <Formik
+        validationSchema={validationSchema}
+        enableReinitialize
+        initialValues={customer}
+        onSubmit={(values) => handleFormSubmit(values)}
+      >
+        {({ handleSubmit, isValid, isSubmitting, dirty }) => (
+          <Form className="ui form" onSubmit={handleSubmit} autoComplete="off">
+            <Header content="פרטי הלקוח" sub color="teal" />
+            <MyTextInput label="חברה" placeholder="חברה" name="company" />
+            <MyTextInput
+              label="שם לקוח"
+              placeholder="שם לקוח"
+              name="customerName"
+            />
+            <MyTextInput
+              label="מייל ראשי"
+              placeholder="מייל ראשי"
+              name="mainEmail"
+            />
+            <MyTextInput
+              label="מספר פלאפון ראשי"
+              placeholder="מספר פלאפון ראשי"
+              name="mainPhoneNumber"
+            />
+            <MyTextInput label="כתובת" placeholder="כתובת" name="address" />
+            <MyTextInput label="חפ" placeholder="חפ" name="hp" />
+            <MySelectInput
+              label="סוג לקוח"
+              options={customerTypeOptions}
+              placeholder="סוג לקוח"
+              name="customerType"
+            />
+            <Header content="פרטי הלקוח- הנהלת חשבונות" sub color="teal" />
+            <MyTextInput
+              label="הנהלת חשבונות-שם לקוח"
+              placeholder="הנהלת חשבונות-שם לקוח"
+              name="bookkeepingName"
+            />
+            <MyTextInput
+              label="הנהלת חשבונות-מייל"
+              placeholder="הנהלת חשבונות-מייל"
+              name="bookkeepingEmail"
+            />
+            <MyTextInput
+              label="הנהלת חשבונות-מספר פלאפון"
+              placeholder="הנהלת חשבונות-מספר פלאפון"
+              name="bookkeepingPhoneNumber"
+            />
+            <MyTextArea
+              label="מידע נוסף"
+              rows={3}
+              placeholder="מידע נוסף"
+              name="moreInfo"
+            />
+            <Button
+              disabled={isSubmitting || !dirty || !isValid}
+              loading={loading}
+              floated="right"
+              positive
+              type="submit"
+              content="שמור"
+            />
+            <Button
+              as={Link}
+              to="/customers"
+              floated="right"
+              type="button"
+              content="ביטול"
+            />
+          </Form>
+        )}
+      </Formik>
     </Segment>
   );
 });
---
+++ client-app/src/app/common/options/customerTypeOptions.ts
@@ -0,0 +1,7 @@
+export const customerTypeOptions = [
+  { text: "פרטי", value: "פרטי" },
+  { text: "עסק", value: "עסק" },
+  { text: "מוסד", value: "עסק" },
+  { text: "דילר", value: "דילר" },
+  { text: "אחר", value: "אחר" },
+];
